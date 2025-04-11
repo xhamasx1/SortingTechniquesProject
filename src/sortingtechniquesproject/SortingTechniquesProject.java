5670c596e51937717f64259a3ed598582412c57e
Hamas Alsharari <alshraryhmas@gmail.com>
2025-04-11 23:50:20 +0300

    Update SortingTechniquesProject.java
    Fixed bubbleSort performance by adding early exit condition.

--- src/sortingtechniquesproject/SortingTechniquesProject.java
+++ src/sortingtechniquesproject/SortingTechniquesProject.java
@@ -4,20 +4,19 @@ import java.util.Scanner;
 
 public class SortingTechniquesProject {
 
-
-      public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("input array size: ");
         int size = scanner.nextInt();
         
         while (size <= 0) {
-        System.out.print(" Enter array size again (must be greater than 0): ");
-        size = scanner.nextInt();
+            System.out.print(" Enter array size again (must be greater than 0): ");
+            size = scanner.nextInt();
 
-        if (size <= 0) {
-        System.out.println("Invalid input. Please enter a number greater than 0.");
-    }
-}
+            if (size <= 0) {
+                System.out.println("Invalid input. Please enter a number greater than 0.");
+            }
+        }
         
         int[] numbers = new int[size];
         
@@ -30,25 +29,21 @@ public class SortingTechniquesProject {
         System.out.println();
         System.out.println("Sorting Techniques:");
         
-
         int[] bubbleSortedNumbers = numbers.clone();
-        bubblesort(bubbleSortedNumbers);
-        System.out.print("bubblesort: ");
+        bubbleSort(bubbleSortedNumbers);
+        System.out.print("bubbleSort: ");
         printArray(bubbleSortedNumbers);
         
-
         int[] quickSortedNumbers = numbers.clone();
         quickSort(quickSortedNumbers, 0, quickSortedNumbers.length - 1);
         System.out.print("quickSort: ");
         printArray(quickSortedNumbers);
         
-
         int[] selectionSortedNumbers = numbers.clone();
         selectionSort(selectionSortedNumbers);
         System.out.print("selectionSort: ");
         printArray(selectionSortedNumbers);
         
-
         int[] insertionSortedNumbers = numbers.clone();
         insertionSort(insertionSortedNumbers);
         System.out.print("insertionSort: ");
@@ -59,7 +54,7 @@ public class SortingTechniquesProject {
         System.out.print("mergeSort: ");
         printArray(mergeSortedNumbers);
     }
-    
+
     public static void printArray(int[] arr) {
         for (int num : arr) {
             System.out.print(num + " ");
@@ -67,11 +62,9 @@ public class SortingTechniquesProject {
         System.out.println();
     }
 
-    
-
-    public static int[] bubblesort(int[] numbers) {
-        boolean swapped = true ;
-        for (int i = 0; i < numbers.length - 1 && swapped; i++) {
+    public static int[] bubbleSort(int[] numbers) {
+        boolean swapped;
+        for (int i = 0; i < numbers.length - 1; i++) {
             swapped = false;
             for (int j = 0; j < numbers.length - i - 1; j++) {
                 if (numbers[j + 1] < numbers[j]) {
@@ -81,25 +74,24 @@ public class SortingTechniquesProject {
                     swapped = true;
                 }
             }
+            if (!swapped) break;
         }
         return numbers;
     }
 
-    
-    public static int[] selectionSort(int arr[]) {
+    public static int[] selectionSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
-            int smallest_element_index = i;
+            int smallestElementIndex = i;
             for (int j = i + 1; j < arr.length; j++)
-                if (arr[j] < arr[smallest_element_index])
-                    smallest_element_index = j;
-            int smallestNumber = arr[smallest_element_index];
-            arr[smallest_element_index] = arr[i];
+                if (arr[j] < arr[smallestElementIndex])
+                    smallestElementIndex = j;
+            int smallestNumber = arr[smallestElementIndex];
+            arr[smallestElementIndex] = arr[i];
             arr[i] = smallestNumber;
         }
-        return arr ;
+        return arr;
     }
 
-    
     public static void insertionSort(int[] data) {
         int n = data.length;
         for (int k = 1; k < n; k++) {
@@ -115,10 +107,11 @@ public class SortingTechniquesProject {
 
     public static int[] mergeSort(int[] inputArray) {
         int size = inputArray.length;
-        if (size < 2){
-            if(size==1)
+        if (size < 2) {
+            if (size == 1)
                 return inputArray;
-            return null;              }
+            return null;
+        }
         int mid = size / 2;
         int leftSize = mid;
         int rightSize = size - mid;
@@ -135,8 +128,8 @@ public class SortingTechniquesProject {
         merge(left, right, inputArray);
         return inputArray;
     }
-    
-public static int[] merge(int[] left, int[] right, int[] arr) {
+
+    public static int[] merge(int[] left, int[] right, int[] arr) {
         int leftSize = left.length;
         int rightSize = right.length;
         int i = 0, j = 0, k = 0;
@@ -162,33 +155,31 @@ public static int[] merge(int[] left, int[] right, int[] arr) {
             j++;
         }
         return arr;
-        
     }
-    
+
     public static int[] quickSort(int[] arr, int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-    return arr;
-}
-
-private static int partition(int[] arr, int low, int high) {
-    int pivot = arr[high];
-    int i = (low - 1);
-    for (int j = low; j < high; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
         }
+        return arr;
     }
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-    return i + 1;
-}}
 
-
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+} 
